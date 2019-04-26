653912faaf8271f51017fd896f4abeaffd08da32
Oscar Vera <50033715+ojvr1528@users.noreply.github.com>
2019-04-26 18:56:21 -0400

    Add files via upload

---
+++ ITS320_CTA1_Option2(1).py
@@ -0,0 +1,8 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from __future__ import division
+a = int(raw_input())
+b = int(raw_input())
+
+print a // b
+print a / b
